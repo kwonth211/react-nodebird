367570331f3a9611c58f76c416f90118675f58ea
kwonth211 <kwonth211@naver.com>
2021-02-11 23:11:42 +0900

    login form not used library

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,13 @@
 import PropTypes from "prop-types";
-import { Menu } from "antd";
+import { Input, Menu, Row, Col } from "antd";
 import Link from "next/link";
+import { useState } from "react";
+
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
+
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState();
   return (
     <div>
       <Menu mode="horizontal">
@@ -16,11 +22,26 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
+
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://github.com/kwonth211"> Made by Kwonth211</a>
+        </Col>
+      </Row>
       {children}
     </div>
   );
